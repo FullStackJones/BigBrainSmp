b50b78334bb7ab10033d81aec4f77949f5a48bdd
FullStackJones <gregoryjones0498@gmail.com>
2024-09-29 19:54:11 +0100

    feat: add gold, silver and copper coins (#1)
    * added gold coin item
    * added silver coin item
    * added copper coin item

--- src/main/java/net/fullstackjones/bigbrainsmpmod/BigBrainSmpMod.java
+++ src/main/java/net/fullstackjones/bigbrainsmpmod/BigBrainSmpMod.java
@@ -1,5 +1,6 @@
 package net.fullstackjones.bigbrainsmpmod;
 
+import net.fullstackjones.bigbrainsmpmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -51,6 +52,8 @@ public class BigBrainSmpMod
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -66,7 +69,12 @@ public class BigBrainSmpMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.GOLDCOIN);
+            event.accept(ModItems.SILVERCOIN);
+            event.accept(ModItems.COPPERCOIN);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/fullstackjones/bigbrainsmpmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.fullstackjones.bigbrainsmpmod.item;
+
+import net.fullstackjones.bigbrainsmpmod.BigBrainSmpMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BigBrainSmpMod.MODID);
+
+    public static final DeferredItem<Item> GOLDCOIN = ITEMS.register(
+            "goldcoin",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SILVERCOIN = ITEMS.register(
+            "silvercoin",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> COPPERCOIN = ITEMS.register(
+            "coppercoin",
+            () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
