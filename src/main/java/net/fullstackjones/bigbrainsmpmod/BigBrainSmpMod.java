d12315ba21a7f30c456d3461ca2261aaaa1fdd2b
FullStackJones <gregoryjones0498@gmail.com>
2024-09-30 08:32:07 +0100

    chore: simplify coin code (#2)
    * Creative tab & Coin Item
    
    + creative tab
    + Coin item type
    + modified Coin registration process
    * code tidy & config

--- src/main/java/net/fullstackjones/bigbrainsmpmod/BigBrainSmpMod.java
+++ src/main/java/net/fullstackjones/bigbrainsmpmod/BigBrainSmpMod.java
@@ -1,23 +1,11 @@
 package net.fullstackjones.bigbrainsmpmod;
 
+import net.fullstackjones.bigbrainsmpmod.item.ModCreativeModeTabs;
 import net.fullstackjones.bigbrainsmpmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -28,12 +16,7 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(BigBrainSmpMod.MODID)
@@ -52,10 +35,9 @@ public class BigBrainSmpMod
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        ModItems.register(modEventBus);
+        ModCreativeModeTabs.register(modEventBus);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+        ModItems.register(modEventBus);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -64,17 +46,6 @@ public class BigBrainSmpMod
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
-        {
-            event.accept(ModItems.GOLDCOIN);
-            event.accept(ModItems.SILVERCOIN);
-            event.accept(ModItems.COPPERCOIN);
-        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/fullstackjones/bigbrainsmpmod/item/ModItems.java
+++ src/main/java/net/fullstackjones/bigbrainsmpmod/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.fullstackjones.bigbrainsmpmod.item;
 
 import net.fullstackjones.bigbrainsmpmod.BigBrainSmpMod;
+import net.fullstackjones.bigbrainsmpmod.Config;
+import net.fullstackjones.bigbrainsmpmod.item.custom.CoinItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -8,18 +10,21 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BigBrainSmpMod.MODID);
+    public static final DeferredItem<Item>[] COINS = new DeferredItem[4];
 
-    public static final DeferredItem<Item> GOLDCOIN = ITEMS.register(
-            "goldcoin",
-            () -> new Item(new Item.Properties()));
+    static {
+        final String[] CoinTypes = new String[]{"coppercoin", "silvercoin", "goldcoin", "pinkcoin"};
+        // todo: config file values shouldn't be null.
+        final int[] CoinValues = Config.coinValues != null ? Config.coinValues : new int[]{1, 9, 81, 729};
 
-    public static final DeferredItem<Item> SILVERCOIN = ITEMS.register(
-            "silvercoin",
-            () -> new Item(new Item.Properties()));
-
-    public static final DeferredItem<Item> COPPERCOIN = ITEMS.register(
-            "coppercoin",
-            () -> new Item(new Item.Properties()));
+        for (int i = 0; i < CoinTypes.length; i++) {
+            String coinType = CoinTypes[i];
+            int coinValue = CoinValues[i];
+            COINS[i] = ITEMS.register(
+                    coinType,
+                    () -> new Item(new CoinItem.Properties().coinType(coinType).coinValue(coinValue)));
+        }
+    }
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
